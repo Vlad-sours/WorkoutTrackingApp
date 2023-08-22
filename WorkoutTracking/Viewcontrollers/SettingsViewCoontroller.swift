e1a57566922533ca96ca5045e3677cef6eea9bd6
Vladimir <xalif80@gmail.com>
2023-08-22 22:02:03 +0200

    add title

---
+++ WorkoutTracking/Viewcontrollers/SettingsViewCoontroller.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  WorkoutTracking
+//
+//  Created by Vlad on 22.8.23..
+//
+
+import UIKit
+
+class SettingsViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+    }
+
+
+}
